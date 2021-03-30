ac3c0b0679484e6eee3896c13b7d00b5b284e763
100kanchik <77804200+100kanchik@users.noreply.github.com>
2021-03-30 13:06:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
                     <ul>
 
                         <li>
-                          <a href="/phpinfo.php">Какая-то шляпа по басурманскиn</a>
+                          <a href="/phpinfo.php">Какая-то шляпа по басурмански</a>
                           (to disable: rm /var/www/phpinfo.php)
                         </li>
                         <li>
